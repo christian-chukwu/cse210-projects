aa41e542e1c63db5ad0bbbde9bc9717a15103128
christian-chukwu <nnachris62@gmail.com>
2025-10-09 18:49:24 +0000

    week 4

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+
+
+        // Customers and addresses
+        Address addr1 = new Address("42 Main St", "Ranger Avenue", "NY", "USA");
+        Customer cust1 = new Customer("Mike Fisher", addr1);
+
+        Address addr2 = new Address("13 Bola Street", "Ajao Estate", "Lagos", "Nigeria");
+        Customer cust2 = new Customer("Serena Bob", addr2);
+
+        // Orders
+        Order order1 = new Order(cust1);
+        order1.AddProduct(new Product("Soap", "SOAP01", 3, 5));
+        order1.AddProduct(new Product("Toothpaste", "TOOTH02", 2, 4));
+
+        Order order2 = new Order(cust2);
+        order2.AddProduct(new Product("Shampoo", "SHAMP03", 5, 2));
+        order2.AddProduct(new Product("Conditioner", "COND04", 4, 3));
+
+        // Display orders
+        order1.DisplayOrder();
+        order2.DisplayOrder();
     }
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        List<Video> videos = new List<Video>();
+
+        // Create first video and add comments
+
+        Video video1 = new Video("Learning C#", "Christian", 300);
+        video1.AddComment(new Comment("John", "Great tutorial!"));
+        video1.AddComment(new Comment("Mike", "Very helpful, thanks!"));
+        video1.AddComment(new Comment("Bissa", "I learned a lot."));
+        videos.Add(video1);
+
+        // Create second video and add comments
+
+        Video video2 = new Video("Cooking Jollof Rice", "Olivia", 450);
+        video2.AddComment(new Comment("Chima", "Looks delicious!"));
+        video2.AddComment(new Comment("Sophia", "Can't wait to try this recipe."));
+        video2.AddComment(new Comment("Donatus", "Best recipe so far."));
+        videos.Add(video2);
+
+        // Create third video and add comments
+
+        Video video3 = new Video("C# Design Patterns", "Shantel", 600);
+        video3.AddComment(new Comment("Ebuka", "Design patterns are so useful."));
+        video3.AddComment(new Comment("Joy", "Thanks for the good examples."));
+        video3.AddComment(new Comment("Chisom", "I would love to see more videos like this."));
+        videos.Add(video3);
+
+        // Display all videos and their comments
+        foreach (var video in videos)
+        {
+            video.Display();
+        }
     }
 }
+
+
